8689e8fbf76534d800181721e57b85d7242d7d3c
Matgenius04 <Matgenius04@gmail.com>
2021-05-16 00:10:23 -0500

    Update buildRust.sh

--- buildRust.sh
+++ buildRust.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+FILE=./target/debug/libWasmtime_embedding
+
 cd ./src/main/Wasmtime-embedding
 cargo build
-cp -i ./target/debug/libWasmtime_embedding.dylib ../resources
+if [ -f "$FILE.so" ]; then
+  cp -i "$FILE.so" ../resources
+elif [ -f "$FILE.dylib" ]; then
+  cp -i "$FILE.dylib" ../resources
+elif [ -f "$FILE.dll" ]; then
+  cp -i "$FILE.dll" ../resources
+else
+  echo "$FILE does not exist"
+fi
